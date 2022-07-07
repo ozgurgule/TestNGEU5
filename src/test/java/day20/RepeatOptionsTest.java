38ed402beeef8755942937c3fb8efaab90f1983e
OzgurGuler <ozgur.guler.20@hotmail.com>
2022-07-07 12:25:32 +0300

    Last test

---
+++ src/test/java/day20/RepeatOptionsTest.java
@@ -0,0 +1,14 @@
+package day20;
+
+import Utilities.TestBase;
+
+public class RepeatOptionsTest extends TestBase {
+    /* Verify Radio Buttons
+    Open browser
+    Login as driver
+    Go to Activities Calendar Event
+    Click on create calendar events
+    Click on repeat
+    Verify that repeat every days is checked
+    verify that repeat weekday is not checked */
+}
