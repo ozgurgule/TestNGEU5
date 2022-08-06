d995762a0a1d2771a4f3ecddb9ad1faa83331ffb
OzgurGuler <ozgur.guler.20@hotmail.com>
2022-08-06 20:51:35 +0200

    all project TestNG

---
+++ src/test/java/day24/PracticeThan.java
@@ -0,0 +1,17 @@
+package day24;
+
+public class PracticeThan {
+    public static void main(String[] args) {
+
+      String abc = "Muaz is a student of Cybertek";
+
+
+      while (abc.length()<100) {
+          System.out.println(abc);
+          abc=abc+" Geceler geceler hain geceler";
+
+        }
+
+    }
+
+}
